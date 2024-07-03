c7309e1873fbc5d123facbb6dbe5652947085b0d
zakarya3 <zakaria.aanni@gmail.com>
2024-07-03 19:43:04 +0100

    fix

--- src/store/apiService.ts
+++ src/store/apiService.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const access_token = localStorage.getItem("access_token");
 const apiClient = axios.create({
   baseURL: "https://taskmanagebackend-l0jp.onrender.com/api",
   withCredentials: true,
+  headers: {
+    Authorization: "Bearer " + access_token,
+  },
 });
 
 export default apiClient;
